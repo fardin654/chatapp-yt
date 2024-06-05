c78a443aab52fcac70a28d1167e173c5d5d038cc
fardin654 <fardinmd654@gmail.com>
2024-06-05 23:52:04 +0530

    Chhanges

--- backend/frontend/src/components/Contacts.jsx
+++ backend/frontend/src/components/Contacts.jsx
@@ -13,7 +13,7 @@ function Contacts({ contacts, currentUser,changeChat}) {
   const [onlineUsers, setOnlineUsers] = useState({});
 
   useEffect(()=>{
-    const socket = io(host);
+    const socket = io("https://chatapp-yt-c4h4.onrender.com");
     socket.on("onlineUsers",(data)=>{
       setOnlineUsers(data);
     })
