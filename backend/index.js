8db98e918e5c99a59de1035bb1128b8401748065
fardin654 <fardinmd654@gmail.com>
2024-06-05 23:27:10 +0530

    app completed

--- backend/index.js
+++ backend/index.js
@@ -14,23 +14,20 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/auth",userRoutes); 
 app.use("/api/messages",messagesRoute); 
-
-// --------------------------deployment------------------------------
-
-const __dirname1 = path.resolve(__dirname, '..'); 
+// -----Code for Deployement---
+const __dirname1 = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "public", "build")));
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "public", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
     res.send("API is running..");
   });
 }
-//--------------------
 
 mongoose.connect(process.env.MONGO_URL
 ,{
